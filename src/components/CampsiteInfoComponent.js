d5223a4cb4554bc92ab0fdd98dc58aff1b0696b6
Morgan Sissom <masissom@gmail.com>
2020-06-05 10:38:08 -0400

    Header n Footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,51 +1,51 @@
-//not even sure if these imports are needed but nothing, and I do mean nothing was compiling. This combination gave me the least amount of errors.
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-class CampsiteInfo extends Component {
-    renderCampsite = (campsite) => (
-        <div key={this.props.campsite.id} className="col-md-5 m-1">
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+function RenderCampsite({ campsite }) {
+    return (
+        <div className="col-md-5 m-1">
             <Card>
-                <CardImg top src={this.props.campsite.image} alt={this.props.campsite.name} />
+                <CardImg top src={campsite.image} alt={campsite.name} />
                 <CardBody>
-                    <CardTitle>{this.props.campsite.name}</CardTitle>
-                    <CardText>{this.props.campsite.description}</CardText>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
                 </CardBody>
             </Card>
         </div>
     );
+}
 
-    renderComments = (comments) => {
-        if (comments) {
-            return (
-                <div className="col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {comments.map(comment => {
-                        return (
-                            <div key={comment.id}>
-                                <p>{comment.text}<br />
+function RenderComments({comments}) {
+    if (comments) {
+        return (
+            <div className="col-md-5 m-1">
+                <h4>Comments</h4>
+                {comments.map(comment => {
+                    return (
+                        <div key={comment.id}>
+                            <p>{comment.text}<br />
                             -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                            </div>
-                        );
-                    })}
-                </div>
-            );
-        }
-        return <div />
+                        </div>
+                    );
+                })}
+            </div>
+        );
     }
+    return <div />
+}
 
-    render() {
-        if (this.props.campsite) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        {this.renderCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
-                    </div>
+function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
                 </div>
-            )
-        }
-        return <div />
-    };
+            </div>
+        );
+    }
+    return <div />
 };
 
 export default CampsiteInfo;

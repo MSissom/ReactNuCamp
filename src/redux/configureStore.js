3423457880aed7234b0120977baf2678b14f4cd3
Morgan Sissom <masissom@gmail.com>
2020-06-16 16:52:05 -0400

    React Redux Forms Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { Campsites } from './campsites';
 import { Promotions } from './promotions';
 import { Comments } from './comments';
 import { Partners } from './partners';
+import { InitialFeedback } from './Forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -12,7 +14,10 @@ export const ConfigureStore = () => {
            campsites: Campsites,
            promotions: Promotions,
            comments: Comments,
-           partners: Partners 
+           partners: Partners, 
+           ...createForms({
+               feedbackForm: InitialFeedback
+           })
         }),
         applyMiddleware(thunk, logger)
     );
